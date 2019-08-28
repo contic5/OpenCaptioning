e132f2c1065aa9ffac3318a8d574552261bff9f1
CJ Conti <cjc2818cjc@aol.com>
2019-08-28 15:19:42 -0400

    change2818

--- addmeme.php
+++ addmeme.php
@@ -14,7 +14,7 @@
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  // Check if image file is a actual image or fake image
+  // Check if image file is a actual image or fake image. Useful.
   if(isset($_POST["submit"]))
   {
       $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
